446536a01c4fdcffddae3b203035fc5f178c7ff6
Diligre <diligre124121@gmail.com>
2020-07-04 00:38:58 +0300

    init

--- api/src/main/java/com/projectx/projectx/users/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projectx.projectx.users;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<AdminModel,Long> {
-
-
-
-}
